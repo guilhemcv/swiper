3b9a09cdf272af5c07db690791192d698585e03a
Coucoralie <c.cocourant@gmail.com>
2021-10-27 17:20:31 +0200

    styles created

--- src/components/Carte/Carte.js
+++ src/components/Carte/Carte.js
@@ -4,27 +4,26 @@ import "./Carte.css";
 function Carte() {
   return (
     <div className="carte">
-      <div>
+      <div className="info">
         <h2> A quoi ça sert ? </h2>
         <p>
           "On fait quoi ce week-end ?" T'en as marre de cette question ?! Pas de
-          panique ! Swiper est là pour toi !Tu retouveras des propostions
+          panique ! Swiper est là pour toi! Tu retouveras des propostions
           d'activités par catégorie : Lieux culturels, Parcs, Sport et Loisirs,
-          Restaurants et même les parkings les plus proches de toi !
+          Restaurants et même les parkings les plus proches de toi! Tu pourras
+          retrouver les activités que tu as aimé dans tes Favoris. Un plan de la
+          ville te permettra de retrouver ces lieux.
         </p>
       </div>
-      <div>
+      <div className="fonctionnement">
         <h2> Comment ça fonctionne ? </h2>
         <p>
-          Tu peux commencer par créer un compte via ton compte Google ou
-          Facebook.Ensuite tu peux te diriger vers la page Swipe et sélectionner
-          la catégorie qui t'intéresse : une photo et un petit texte descriptif
-          s'affiche, de là tu peux swiper à droite si l'activité t'intéresse
-          sinon swipe à gauche. Si tu cliques si l'activité proposée tu seras
-          rediriger vers la Carte pour situer le lieux. Sur la Carte tu pourras
-          sélectionner les catégories qui t'intéresse pour retrouver leur
-          emplacement. Sur la page Favoris tu retrouveras les activés que tu as
-          sélectionnées classées par catégorie.
+          Tu peux commencer par créer un compte.Ensuite tu peux te diriger vers
+          la page Swipe et sélectionner la catégorie qui t'intéresse : une
+          activité te sera proposée, de là tu peux swiper à droite si cette
+          propostion t'intéresse sinon swipe à gauche. Tu pourras les retrouver
+          sur la page Favoris. Si tu cliques sur l'activité proposée tu seras
+          redirigé vers un plan pour situer le lieu.
         </p>
       </div>
     </div>
