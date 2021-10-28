da4dc4b958e527f7124a49b5d3b0cc0b393feece
Coucoralie <c.cocourant@gmail.com>
2021-10-29 00:33:11 +0200

    style of desktop created

--- src/components/Avis/Avis.js
+++ src/components/Avis/Avis.js
@@ -4,34 +4,39 @@ import Images from "./Images/coralie.png";
 
 function Avis() {
   return (
-    <div className="avis">
-      <h2>Ils parlent de nous</h2>
-      <div className="avis1">
-        <img src={Images} alt="Portrait Coralie" class="photo" />
-        <p>
-          J'adore utiliser Swiper pour organiser mes week-ends! Je ne peux plus
-          m'en passer!!
-        </p>
+    <div>
+      <div className="titre">
+        <h2>Ils parlent de nous</h2>
       </div>
-      <div className="avis2">
-        <img src={Images} alt="Portrait Lucas" class="photo" />
-        <p>
-          Je l'ai utilisé pour découvrir Nantes: Super appli', très complète!
-        </p>
-      </div>
-      <div className="avis3">
-        <img src={Images} alt="Portrait Maxime" class="photo" />
-        <p>
-          Avec notre groupe d'amis, on l'utilise tous les week-ends! On est
-          fan!!
-        </p>
-      </div>
-      <div className="avis4">
-        <img src={Images} alt="Portrait Guilhem" class="photo" />
-        <p>
-          Nantais depuis toujours, Swiper est tellement bien conçu que j'ai
-          l'impression de (re)-découvrir ma ville!
-        </p>
+      <div className="avis">
+        <div className="avis1">
+          <img src={Images} alt="Portrait Coralie" class="photo" />
+          <p>
+            J'adore utiliser Swiper pour organiser mes week-ends! Je ne peux
+            plus m'en passer!!
+          </p>
+        </div>
+        <div className="avis2">
+          <img src={Images} alt="Portrait Lucas" class="photo" />
+          <p>
+            Grâce à Swiper je re-découvre ma ville tous les week-ends!! Trop
+            bien conçu!
+          </p>
+        </div>
+        <div className="avis3">
+          <img src={Images} alt="Portrait Maxime" class="photo" />
+          <p>
+            Je l'ai utilisé pour découvrir Nantes: Super appli', très complète!
+            Hyper fan!
+          </p>
+        </div>
+        <div className="avis4">
+          <img src={Images} alt="Portrait Guilhem" class="photo" />
+          <p>
+            Avec notre groupe d'amis, on l'utilise tous les week-ends! On est
+            fan!!
+          </p>
+        </div>
       </div>
     </div>
   );
