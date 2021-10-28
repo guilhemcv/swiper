a1a6e0fb184acaea61732b4b7c7f4bdc3b2ed6da
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-10-28 16:01:33 +0200

    mise a jour style googlemap

--- src/components/GoogleMapSelection/GoogleMapSelection.js
+++ src/components/GoogleMapSelection/GoogleMapSelection.js
@@ -3,44 +3,55 @@ import "./GoogleMapSelection.css";
 
 function GoogleMapSelection() {
   return (
-    <div>
+    <div className="barreRecherche">
       <div className="selection-recherche">
         <label forHtml="name">
-          <h4>Rechercher une adresse :</h4>
+          <h3>Rechercher une adresse :</h3>
         </label>
         <br />
         <input className="input-recherche" type="text" id="name" name="name" />
       </div>
-      <div className="selection"></div>
-      <div className="inputLabel">
-        <input className="input" type="checkbox" id="parking" name="parking" />
-        <label for="parking">Parkings</label>
-      </div>
-      <div className="inputLabel">
-        <input
-          className="input"
-          type="checkbox"
-          id="restaurant"
-          name="restaurant"
-        />
-        <label for="restaurant">Restaurants</label>
-      </div>
-      <div className="inputLabel">
-        <input className="input" type="checkbox" id="sport" name="sport" />
-        <label for="sport">Sport et loisirs</label>
-      </div>
-      <div className="inputLabel">
-        <input
-          className="input"
-          type="checkbox"
-          id="culturel"
-          name="culturel"
-        />
-        <label for="culturel">Lieux culturels</label>
-      </div>
-      <div className="inputLabel">
-        <input className="input" type="checkbox" id="cinemas" name="cinemas" />
-        <label for="cinemas">Cinémas</label>
+      <div className="selection">
+        <div className="inputLabel">
+          <input
+            className="input"
+            type="checkbox"
+            id="parking"
+            name="parking"
+          />
+          <label for="parking">Parkings</label>
+        </div>
+        <div className="inputLabel">
+          <input
+            className="input"
+            type="checkbox"
+            id="restaurant"
+            name="restaurant"
+          />
+          <label for="restaurant">Restaurants</label>
+        </div>
+        <div className="inputLabel">
+          <input className="input" type="checkbox" id="sport" name="sport" />
+          <label for="sport">Sport et loisirs</label>
+        </div>
+        <div className="inputLabel">
+          <input
+            className="input"
+            type="checkbox"
+            id="culturel"
+            name="culturel"
+          />
+          <label for="culturel">Lieux culturels</label>
+        </div>
+        <div className="inputLabel">
+          <input
+            className="input"
+            type="checkbox"
+            id="cinemas"
+            name="cinemas"
+          />
+          <label for="cinemas">Cinémas</label>
+        </div>
       </div>
     </div>
   );
