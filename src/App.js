05291172d51eedc1691a80e583b64528782cefc1
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-10-29 22:10:58 +0200

    creation branche fectch google map

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Swipe from "./container/swipe";
 function App() {
   return (
     <div className="App">
-      <Accueil />
-      <Swipe />
+      {/*       <Accueil />
+      <Swipe /> */}
       <Map />
-      <Favoris />
+      {/*       <Favoris /> */}
     </div>
   );
 }
