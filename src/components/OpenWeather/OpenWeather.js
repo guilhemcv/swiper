e69f18eb41f56dcfc72208474ce4fd56d4633193
Coucoralie <c.cocourant@gmail.com>
2021-11-04 17:35:30 +0100

    affichage de la température

--- src/components/OpenWeather/OpenWeather.js
+++ src/components/OpenWeather/OpenWeather.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import clouds from "../Footer/icone-de-nuage.png";
+import rain from "../Footer/icone-de-pluie.png";
+import clearSky from "../Footer/icone-de-soleil.png";
 
 function OpenWeather() {
   const [temp, setTemp] = React.useState(null);
-  const [sky, setSky] = React.useState("soleil");
+  const [sky, setSky] = React.useState(null);
   const weatherImg = {
-    Clouds: "../Footer/icone-de-nuage.png",
-    soleil: "",
-    pluie: "../Footer/icone-de-pluie.png",
+    Clouds: { clouds },
+    Rain: { rain },
+    ClearSky: { clearSky },
   };
 
   useEffect(() => {
@@ -20,8 +23,8 @@ function OpenWeather() {
       .then((data) => {
         setTemp(data.main.temp);
         console.log(temp);
-        setSky(data.weather[0].main);
-        console.log(data.weather[0]);
+        setSky(data.weather[0].icon);
+        console.log(data.weather[0].icon);
       });
   }, []);
 
@@ -30,13 +33,17 @@ function OpenWeather() {
       <div className="footer-meteo">
         <div className="footer-meteo-titre">
           <h3>Nantes</h3>
-          {temp != null ? <h3>{temp}</h3> : ""}
+          {temp != null ? <h3>{Math.round(temp)} °</h3> : ""}
         </div>
-        <img
-          className="footer-image-meteo"
-          src={weatherImg[sky]}
-          alt="logo nuage"
-        />
+        {sky != null ? (
+          <img
+            src={`http://openweathermap.org/img/wn/{setSky(
+              data.weather[0].icon
+            )}@2x.png`}
+          />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
