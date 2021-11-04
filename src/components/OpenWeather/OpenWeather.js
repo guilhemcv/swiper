13c505152457c406f58b1ea8c7eb01ccc4efb54a
Coucoralie <c.cocourant@gmail.com>
2021-11-04 14:35:14 +0100

    openweather api called

--- src/components/OpenWeather/OpenWeather.js
+++ src/components/OpenWeather/OpenWeather.js
@@ -1,34 +1,44 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import nuage from "../Footer/icone-de-nuage.png";
 
 function OpenWeather() {
-  const [weather, setWeather] = React.useState(true);
+  const [temp, setTemp] = React.useState(null);
+  const [sky, setSky] = React.useState("soleil");
+  const weatherImg = {
+    Clouds: "../Footer/icone-de-nuage.png",
+    soleil: "",
+    pluie: "../Footer/icone-de-pluie.png",
+  };
+
   useEffect(() => {
+    console.log("hello");
     axios
       .get(
         "http://api.openweathermap.org/data/2.5/weather?q=Nantes,fr&APPID=2aed266dbd13eb51768f8ca86b3086ae&units=metric"
       )
       .then((response) => response.data)
       .then((data) => {
-        console.log(data.temp);
-        setWeather(data);
+        setTemp(data.main.temp);
+        console.log(temp);
+        setSky(data.weather[0].main);
+        console.log(data.weather[0]);
       });
   }, []);
 
   return (
-    weather && (
-      <div className="weather">
-        <div className="footer-meteo">
-          <div className="footer-meteo-titre">
-            <h3>Nantes</h3>
-            <h3>22°</h3>
-            <p>{weather.main.temp}</p>
-          </div>
-          <img className="footer-image-meteo" src={nuage} alt="logo nuage" />
+    <div className="weather">
+      <div className="footer-meteo">
+        <div className="footer-meteo-titre">
+          <h3>Nantes</h3>
+          {temp != null ? <h3>{temp}</h3> : ""}
         </div>
+        <img
+          className="footer-image-meteo"
+          src={weatherImg[sky]}
+          alt="logo nuage"
+        />
       </div>
-    )
+    </div>
   );
 }
 
