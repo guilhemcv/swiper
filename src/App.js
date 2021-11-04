fe2ef6329add5d88173b2da756c54bb9e2e91e78
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-04 12:25:45 +0100

    burger fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Accueil from "./container/accueil";
 import Favoris from "./container/favoris";
 import Map from "./container/map";
 import Swipe from "./container/swipe";
-import MenuBurger from "./components/MenuBurger/MenuBurger";
+import "./components/MenuBurger/MenuBurger.css";
 
 function App() {
   return (
     <div className="App">
-      <MenuBurger />
       <Router>
+        <div>
+          <input id="burger" type="checkbox" />
+          <label id="label-burger" htmlFor="burger">
+            <span id="burger-top"></span>
+            <span id="burger-middle"></span>
+            <span id="burger-bottom"></span>
+          </label>
+          <nav id="nav-header">
+            <ul>
+              <li>
+                <Link to="/" className="a">
+                  Accueil
+                </Link>
+              </li>
+              <li>
+                <Link to="/swipe" className="a" htmlFor="burger">
+                  Swipe
+                </Link>
+              </li>
+              <li>
+                <Link to="/map" className="a">
+                  Carte
+                </Link>
+              </li>
+              <li>
+                <Link to="/favoris" className="a">
+                  Favoris
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <Switch>
           <Route path="/swipe" component={Swipe} />
           <Route path="/map" component={Map} />
--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -1,43 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import "./MenuBurger.css";
 
 function MenuBurger() {
   return (
-    <Router>
-      <div>
-        <input id="burger" type="checkbox" />
-        <label id="label-burger" htmlFor="burger">
-          <span id="burger-top"></span>
-          <span id="burger-middle"></span>
-          <span id="burger-bottom"></span>
-        </label>
-        <nav id="nav-header">
-          <ul>
-            <li>
-              <Link to="/" className="a" htmlFor="burger">
-                Accueil
-              </Link>
-            </li>
-            <li>
-              <Link to="/swipe" className="a" htmlFor="burger">
-                Swipe
-              </Link>
-            </li>
-            <li>
-              <Link to="/map" className="a">
-                Carte
-              </Link>
-            </li>
-            <li>
-              <Link to="/favoris" className="a">
-                Favoris
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </Router>
+    <div>
+      <input id="burger" type="checkbox" />
+      <label id="label-burger" htmlFor="burger">
+        <span id="burger-top"></span>
+        <span id="burger-middle"></span>
+        <span id="burger-bottom"></span>
+      </label>
+      <nav id="nav-header">
+        <ul>
+          <li>
+            <Link to="/" className="a">
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link to="/swipe" className="a" htmlFor="burger">
+              Swipe
+            </Link>
+          </li>
+          <li>
+            <Link to="/map" className="a">
+              Carte
+            </Link>
+          </li>
+          <li>
+            <Link to="/favoris" className="a">
+              Favoris
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
