b44b04d7fa289147be17719b9ddd22a710fdfc67
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-05 22:03:53 +0100

    transfert fetch composant 'GoogleMap' sur composant parent 'Map'

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,12 +1,12 @@
 /* eslint-disable prefer-destructuring */
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import axios from "axios";
 import "./GoogleMap.css";
-import imageParcs from "./park.png";
-import imageParking from "./Parking.png";
-import imageMusee from "./Musee.png";
-import imageCinema from "./cinema.png";
+import imageParcs from "../../Assets/Images/Markers/park.png";
+import imageParking from "../../Assets/Images/Markers/parking.png";
+import imageMusee from "../../Assets/Images/Markers/musee.png";
+import imageCinema from "../../Assets/Images/Markers/cinema.png";
+import imageRestaurant from "../../Assets/Images/Markers/restaurant.png";
 
 /* API Google */
 const key = process.env.REACT_APP_API_KEY;
@@ -61,25 +61,37 @@ function MarkerCinema() {
     </div>
   );
 }
+/* Marker pour les restaurant */
+function MarkerRestaurant() {
+  return (
+    <div
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img src={imageRestaurant} width="35px" alt="musee"></img>
+    </div>
+  );
+}
 
 /* fonction affichage de la Google Map */
 class GoogleMap extends React.Component {
-  constructor(props) {
+  /*   constructor(props) {
     super(props);
     this.state = {
       markersParking: [],
       markersParc: [],
       markersMusee: [],
       markersCinema: [],
-    };
-  }
+      animation: 2,
+    }; 
+  } */
 
   /* 
   //UseEffect pour affichage des markers sur la Google Map
   */
 
-  componentDidMount() {
-    /* fetch pour les parkings */
+  /*   componentDidMount() {
     axios
       .get(
         "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes&q=&rows=100&facet=libcategorie&facet=libtype&facet=acces_pmr&facet=service_velo&facet=stationnement_velo&facet=stationnement_velo_securise&facet=moyen_paiement"
@@ -93,7 +105,6 @@ class GoogleMap extends React.Component {
           markersParking: parkingData,
         });
       });
-    /* fetch pour les parcs */
     axios
       .get(
         "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&rows=100&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique"
@@ -106,7 +117,6 @@ class GoogleMap extends React.Component {
           markersParc: parcData,
         });
       });
-    /* fetch pour les musées */
     axios
       .get(
         "https://data.opendatasoft.com/api/records/1.0/search/?dataset=244400404_equipements-publics-nantes-metropole%40nantesmetropole&q=&rows=100&facet=theme&facet=categorie&facet=type&facet=commune&refine.categorie=Mus%C3%A9e%2C+Ch%C3%A2teau&refine.commune=Nantes"
@@ -131,7 +141,7 @@ class GoogleMap extends React.Component {
           markersCinema: cinemaData,
         });
       });
-  }
+  } */
 
   /* Définir le center de la carte par défaut */
   static defaultProps = {
@@ -145,36 +155,46 @@ class GoogleMap extends React.Component {
   render() {
     /* Affichage des markers selon disponibilites des données dans l'API */
     let googleMarkersParking = [];
-    if (this.state.markersParking.length > 0) {
-      googleMarkersParking = this.state.markersParking.map((marker) =>
+    if (this.props.parking.length > 0) {
+      googleMarkersParking = this.props.parking.map((marker) =>
         marker ? <MarkerParking lat={marker[0]} lng={marker[1]} /> : <div></div>
       );
     }
     let googleMarkersParc = [];
-    if (this.state.markersParc.length > 0) {
-      googleMarkersParc = this.state.markersParc.map((marker) =>
+    if (this.props.parc.length > 0) {
+      googleMarkersParc = this.props.parc.map((marker) =>
         marker ? <MarkerParc lat={marker[0]} lng={marker[1]} /> : <div></div>
       );
     }
     let googleMarkersMusee = [];
-    if (this.state.markersMusee.length > 0) {
-      googleMarkersMusee = this.state.markersMusee.map((marker) =>
+    if (this.props.musee.length > 0) {
+      googleMarkersMusee = this.props.musee.map((marker) =>
         marker ? <MarkerMusee lat={marker[1]} lng={marker[0]} /> : <div></div>
       );
     }
     let googleMarkersCinema = [];
-    if (this.state.markersCinema.length > 0) {
-      googleMarkersCinema = this.state.markersCinema.map((marker) =>
+    if (this.props.cinema.length > 0) {
+      googleMarkersCinema = this.props.cinema.map((marker) =>
         marker ? <MarkerCinema lat={marker[1]} lng={marker[0]} /> : <div></div>
+      );
+    }
+    let googleMarkersRestaurant = [];
+    if (this.props.restaurant.length > 0) {
+      googleMarkersRestaurant = this.props.restaurant.map((marker) =>
+        marker ? (
+          <MarkerRestaurant lat={marker[1]} lng={marker[0]} />
+        ) : (
+          <div></div>
+        )
       );
     }
     return (
       <div className="googlemap">
         <GoogleMapReact
           options={(map) => ({ mapTypeId: map.MapTypeId.HYBRID })}
-          bootstrapURLKeys={{
+          /*           bootstrapURLKeys={{
             key,
-          }}
+          }} */
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
@@ -182,6 +202,7 @@ class GoogleMap extends React.Component {
           {googleMarkersParc}
           {googleMarkersMusee}
           {googleMarkersCinema}
+          {googleMarkersRestaurant}
         </GoogleMapReact>
       </div>
     );
--- src/container/map.js
+++ src/container/map.js
@@ -1,21 +1,110 @@
 /* eslint-disable import/no-cycle */
 import "./map.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import GoogleMap from "../components/GoogleMap/GoogleMap";
 import GoogleMapSelection from "../components/GoogleMapSelection/GoogleMapSelection";
 import Footer from "../components/Footer/Footer";
 
 function Map() {
+  /* useState pour les Fetch des APIS */
+  const [parking, setParking] = useState("");
+  const [parc, setParc] = useState("");
+  const [musee, setMusee] = useState("");
+  const [cinema, setCinema] = useState("");
+  const [restaurant, setRestaurant] = useState("");
+
+  /* useEffect pour les Fetch des APIS */
+  useEffect(() => {
+    getParking();
+    getParc();
+    getMusee();
+    getCinema();
+    getRestaurant();
+  }, []);
+
+  /* Fetch API pour markers parking */
+  const getParking = () => {
+    axios
+      .get(
+        "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes&q=&rows=100&facet=libcategorie&facet=libtype&facet=acces_pmr&facet=service_velo&facet=stationnement_velo&facet=stationnement_velo_securise&facet=moyen_paiement"
+      )
+      .then((response) => {
+        const parkingData = response.data.records.map(
+          (record) => record.fields.location
+        );
+        setParking(parkingData);
+      });
+  };
+
+  /* Fetch API pour markers parc */
+  const getParc = () => {
+    axios
+      .get(
+        "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&rows=100&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique"
+      )
+      .then((response) => {
+        const parcData = response.data.records.map(
+          (record) => record.fields.location
+        );
+        setParc(parcData);
+      });
+  };
+
+  /* Fetch API pour markers musées */
+  const getMusee = () => {
+    axios
+      .get(
+        "https://data.opendatasoft.com/api/records/1.0/search/?dataset=244400404_equipements-publics-nantes-metropole%40nantesmetropole&q=&rows=100&facet=theme&facet=categorie&facet=type&facet=commune&refine.categorie=Mus%C3%A9e%2C+Ch%C3%A2teau&refine.commune=Nantes"
+      )
+      .then((response) => {
+        const museeData = response.data.records.map(
+          (record) => record.fields.geo_shape.coordinates
+        );
+        setMusee(museeData);
+      });
+  };
+
+  /* Fetch API pour markers cinémas */
+  const getCinema = () => {
+    axios
+      .get(
+        "https://data.opendatasoft.com/api/records/1.0/search/?dataset=244400404_equipements-publics-nantes-metropole%40nantesmetropole&q=&rows=100&facet=theme&facet=categorie&facet=type&facet=commune&refine.categorie=Mus%C3%A9e%2C+Ch%C3%A2teau&refine.commune=Nantes"
+      )
+      .then((response) => {
+        const cinemaData = response.data.records.map(
+          (record) => record.fields.geo_shape.coordinates
+        );
+        setCinema(cinemaData);
+      });
+  };
+
+  /* Fetch API pour markers restaurant */
+  const getRestaurant = () => {
+    axios
+      .get(
+        "https://data.opendatasoft.com/api/records/1.0/search/?dataset=793866443_restaurants-en-loire-atlantique%40loireatlantique&q=&rows=300&facet=type&facet=categorie&facet=commune&facet=modepaiement&facet=departement&refine.commune=NANTES"
+      )
+      .then((response) => {
+        const restaurantData = response.data.records.map(
+          (record) => record.geometry.coordinates
+        );
+        setRestaurant(restaurantData);
+      });
+  };
+
   return (
     <div className="map">
-      {/* Menu et Navigation --------------------------------- */}
-
-      {/* ---------------------------------------------------- */}
-
       {/* Carte + sélection catégorie --------------------------------- */}
       <div className="selectionAndMap">
         <GoogleMapSelection />
-        <GoogleMap />
+        <GoogleMap
+          parking={parking}
+          parc={parc}
+          musee={musee}
+          cinema={cinema}
+          restaurant={restaurant}
+        />
       </div>
       {/* ---------------------------------------------------- */}
 
