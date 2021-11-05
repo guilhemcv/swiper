0b1f3061ab57afdf9852c568c2550b11dbb0d9df
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-05 10:39:40 +0100

    ajout marker musée

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -4,12 +4,13 @@ import GoogleMapReact from "google-map-react";
 import axios from "axios";
 import "./GoogleMap.css";
 import imageParcs from "./parks.png";
-import imageParking from "./Parking_8.png";
+import imageParking from "./Parking.png";
+import imageMusee from "./Musee.png";
 
 /* API Google */
 const key = process.env.REACT_APP_API_KEY;
 
-/* Marker pour les restaurants */
+/* Marker pour les parcs */
 function MarkerParc() {
   return (
     <div
@@ -17,7 +18,7 @@ function MarkerParc() {
         transform: "translate(-50%, -50%)",
       }}
     >
-      <img src={imageParcs} width="30px" alt="restaurant"></img>
+      <img src={imageParcs} width="30px" alt="parc"></img>
     </div>
   );
 }
@@ -30,7 +31,20 @@ function MarkerParking() {
         transform: "translate(-50%, -50%)",
       }}
     >
-      <img src={imageParking} width="30px" alt="restaurant"></img>
+      <img src={imageParking} width="30px" alt="parking"></img>
+    </div>
+  );
+}
+
+/* Marker pour les musées */
+function MarkerMusee() {
+  return (
+    <div
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img src={imageMusee} width="30px" alt="musee"></img>
     </div>
   );
 }
@@ -42,10 +56,16 @@ class GoogleMap extends React.Component {
     this.state = {
       markersParking: [],
       markersParc: [],
+      markersMusee: [],
     };
   }
 
+  /* 
+  //UseEffect pour affichage des markers sur la Google Map
+  */
+
   componentDidMount() {
+    /* fetch pour les parkings */
     axios
       .get(
         "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes&q=&rows=100&facet=libcategorie&facet=libtype&facet=acces_pmr&facet=service_velo&facet=stationnement_velo&facet=stationnement_velo_securise&facet=moyen_paiement"
@@ -59,6 +79,7 @@ class GoogleMap extends React.Component {
           markersParking: parkingData,
         });
       });
+    /* fetch pour les parcs */
     axios
       .get(
         "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parcs-jardins-nantes&q=&rows=100&facet=libtype&facet=gardien&facet=jeux_enfants&facet=pataugeoire&facet=sanitaires&facet=sanitaires_handicapes&facet=chiens_autorises&facet=jardin_clos&facet=abris&facet=point_eau_potable&facet=table_pique_nique"
@@ -70,7 +91,19 @@ class GoogleMap extends React.Component {
         this.setState({
           markersParc: parcData,
         });
-        console.log(parcData);
+      });
+    /* fetch pour les musées */
+    axios
+      .get(
+        "https://data.opendatasoft.com/api/records/1.0/search/?dataset=244400404_equipements-publics-nantes-metropole%40nantesmetropole&q=&rows=100&facet=theme&facet=categorie&facet=type&facet=commune&refine.categorie=Mus%C3%A9e%2C+Ch%C3%A2teau&refine.commune=Nantes"
+      )
+      .then((response) => {
+        const museeData = response.data.records.map(
+          (record) => record.fields.geo_shape.coordinates
+        );
+        this.setState({
+          markersMusee: museeData,
+        });
       });
   }
 
@@ -84,6 +117,7 @@ class GoogleMap extends React.Component {
   };
 
   render() {
+    /* Affichage des markers selon disponibilites des données dans l'API */
     let googleMarkersParking = [];
     if (this.state.markersParking.length > 0) {
       googleMarkersParking = this.state.markersParking.map((marker) =>
@@ -96,25 +130,27 @@ class GoogleMap extends React.Component {
         marker ? <MarkerParc lat={marker[0]} lng={marker[1]} /> : <div></div>
       );
     }
-    console.log(googleMarkersParking);
+    let googleMarkersMusee = [];
+    if (this.state.markersMusee.length > 0) {
+      googleMarkersMusee = this.state.markersMusee.map((marker) =>
+        marker ? <MarkerMusee lat={marker[1]} lng={marker[0]} /> : <div></div>
+      );
+    }
     return (
       <div className="googlemap">
         <GoogleMapReact
+          options={(map) => ({ mapTypeId: map.MapTypeId.HYBRID })}
           bootstrapURLKeys={
             {
-              /* key */
+              /* key, */
             }
           }
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          {/* <MarkerRestaurant lat={47.212369} lng={-1.55} />
-          <MarkerRestaurant lat={47.21} lng={-1.5545} />
-          <MarkerRestaurant lat={47.2} lng={-1.55231} />
-          <MarkerRestaurant lat={47.2165} lng={-1.5552} /> */}
           {googleMarkersParking}
           {googleMarkersParc}
-          {/* <MarkerParking lat={47.21} lng={-1.567} /> */}
+          {googleMarkersMusee}
         </GoogleMapReact>
       </div>
     );
