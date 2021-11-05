4989c4fcd9ac22f79f48417b3e2259805e86e999
Lucas Rossolini <lucas.rossolini@gmail.com>
2021-11-05 09:41:19 +0100

    Ajout de la fonction handleIsChecked permettant de fermer le menu quand on clique sur un lien

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React from "react";
 import Logo from "./components/Logo/Logo";
 import MenuBurger from "./components/MenuBurger/MenuBurger";
 import Accueil from "./container/accueil";
@@ -10,12 +11,24 @@ import NotFound from "./components/NotFound/NotFound";
 import "./components/MenuBurger/MenuBurger.css";
 
 function App() {
+  const [isChecked, setIsChecked] = React.useState("");
+
+  function handleIsChecked() {
+    if (isChecked === "") {
+      setIsChecked("checked");
+    } else {
+      setIsChecked("");
+    }
+  }
   return (
     <div className="App">
       <Router>
+        <Link to="/">
+          <Logo />
+        </Link>
         <div>
-          <input id="burger" type="checkbox" />
-          <label id="label-burger" htmlFor="burger">
+          <input id="burger" type="checkbox" checked={isChecked} />
+          <label id="label-burger" htmlFor="burger" onClick={handleIsChecked}>
             <span id="burger-top"></span>
             <span id="burger-middle"></span>
             <span id="burger-bottom"></span>
@@ -23,22 +36,17 @@ function App() {
           <nav id="nav-header">
             <ul>
               <li>
-                <Link to="/" className="a">
-                  Accueil
-                </Link>
-              </li>
-              <li>
-                <Link to="/swipe" className="a" htmlFor="burger">
+                <Link to="/swipe" className="a" onClick={handleIsChecked}>
                   Swipe
                 </Link>
               </li>
               <li>
-                <Link to="/map" className="a">
+                <Link to="/map" className="a" onClick={handleIsChecked}>
                   Carte
                 </Link>
               </li>
               <li>
-                <Link to="/favoris" className="a">
+                <Link to="/favoris" className="a" onClick={handleIsChecked}>
                   Favoris
                 </Link>
               </li>
