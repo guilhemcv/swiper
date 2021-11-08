ab171e04e8733a49469a98d20cf0464e726eb9be
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-08 13:10:42 +0100

    maj page 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./NotFound.css";
+import image404 from "../../Assets/Images/error404.jpg";
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 PAGE NOT FOUND</h1>
+    <div className="div404">
+      <h1 className="title404">
+        Erreur 404 <br /> Page Introuvable !
+      </h1>
+      <img className="image404" src={image404} alt="404" />
     </div>
   );
 }
