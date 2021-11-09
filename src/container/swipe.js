1b3449e823e556b21d2a38f6ed72eb631749df68
Lucas Rossolini <lucas.rossolini@gmail.com>
2021-11-09 14:31:44 +0100

    Changement de couleur du logo en fonction de la page avec context

--- src/container/swipe.js
+++ src/container/swipe.js
@@ -1,10 +1,18 @@
 /* eslint-disable import/no-cycle */
 import "./swipe.css";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Footer from "../components/Footer/Footer";
 import SwipeCard from "../components/Swipe-card/SwipeCard";
+import LogoContext from "../contexts/LogoContext";
 
 function Swipe() {
+  // Utilisation du LogoContext pour gérer la couleur du logo en fonction de la page où on est
+  const { logoColor, setLogoColor } = useContext(LogoContext);
+
+  useEffect(() => {
+    setLogoColor("logo-black");
+    console.log(logoColor);
+  });
   return (
     <div className="swipe-page">
       {/* Menu et Navigation --------------------------------- */}
