b8f324cc395293920773519bc3282ff4c51da5f2
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-10 23:37:42 +0100

    transformation class en hook, et map sur les fetchs pour intégrer les markers sur la map

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable prefer-destructuring */
 import React from "react";
 import GoogleMapReact from "google-map-react";
@@ -21,225 +22,64 @@ import customStylesDark from "./CustomStylesDark";
 /* API Google */
 const key = process.env.REACT_APP_API_KEY;
 
-/* Marker pour les parcs */
-function MarkerParc() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img className="marker" src={imageParcs} width="35px" alt="parc"></img>
-    </div>
-  );
-}
-/* Marker pour les parkings */
-function MarkerParking() {
-  return (
-    <div
-      className="markernothover"
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageParking}
-        width="35px"
-        alt="parking"
-      ></img>
-    </div>
-  );
-}
-/* Marker pour les musées */
-function MarkerMusee() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img className="marker" src={imageMusee} width="35px" alt="musee"></img>
-    </div>
-  );
-}
-/* Marker pour les cinémas */
-function MarkerCinema() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img className="marker" src={imageCinema} width="35px" alt="cinema"></img>
-    </div>
-  );
-}
-/* Marker pour les restaurant */
-function MarkerRestaurant() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageRestaurant}
-        width="35px"
-        alt="restaurant"
-      ></img>
-    </div>
-  );
-}
-/* Marker pour les salles de spectacle */
-function MarkerSpectacle() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageSpectacle}
-        width="35px"
-        alt="spectacle"
-      ></img>
-    </div>
-  );
-}
-/* Marker pour les piscines */
-function MarkerPiscine() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imagePiscine}
-        width="35px"
-        alt="piscine"
-      ></img>
-    </div>
-  );
-}
-/* Marker pour les bicloo */
-function MarkerBicloo() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img className="marker" src={imageBicloo} width="35px" alt="bicloo"></img>
-    </div>
-  );
-}
-/* Marker pour les bicloo */
-function MarkerMarguerite() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageMarguerite}
-        width="35px"
-        alt="marguerite"
-      ></img>
-    </div>
-  );
-}
-/* Marker pour le sport de plein air */
-function MarkerSport() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageSport}
-        width="35px"
-        alt="marguerite"
-      ></img>
-    </div>
-  );
-}
-
-function Marker() {
-  return (
-    <div
-      style={{
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <img
-        className="marker"
-        src={imageParking}
-        width="60px"
-        alt="recherche"
-      ></img>
-    </div>
-  );
-}
+function Mar() {
+  return <div></div>
+};
 
 /* fonction affichage de la Google Map */
-class GoogleMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-    this.markers = this.props.markers;
-    this.markers.map((marker) => console.log(marker));
-    this.togglePopup = this.togglePopup.bind(this);
+function GoogleMap({ changeTheme, markers }) {
+  const center = { lat: 47.212369, lng: -1.55 };
+
+
+
+  function Marker() {
+    for (let i = 0; i < markers.length; i += 1) {
+      if (markers[i].type === "parking") {
+        return (
+          <div
+            style={{
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <img
+              className="marker"
+              src={imageParking}
+              alt="parking"
+              width="35px"
+            />
+          </div>
+        );
+      }
+      if (markers[i].type === "parcs") {
+        return (
+          <div
+            style={{
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <img className="marker" src={imageParcs} alt="parcs" width="35px" />
+          </div>
+        );
+      }
+    }
   }
 
-  /* fonction affichage popup sur map */
-  togglePopup = () => {
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }));
-  };
-
-  /* Définir le center de la carte par défaut */
-  static defaultProps = {
-    center: {
-      lat: 47.212369,
-      lng: -1.55,
-    },
-    zoom: 13,
-  };
-
-  render() {
-    // eslint-disable-next-line no-return-assign
-    return (
-      <div className="googlemap">
-        <GoogleMapReact
-          options={
-            this.props.changeTheme
-              ? { styles: customStylesDark }
-              : { styles: customStylesLite }
-          }
-          /* bootstrapURLKeys={{ key }} */
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          onClick={() => this.togglePopup()}
-        >
-          {/* {this.markers.map((marker) => (
-            <Marker lat={marker.coordonnees[0]} lng={marker.coordonnees[1]} />
-          ))} */}
-        </GoogleMapReact>
-      </div>
-    );
-  }
+  return (
+    <div className="googlemap">
+      <GoogleMapReact
+        options={
+          changeTheme
+            ? { styles: customStylesDark }
+            : { styles: customStylesLite }
+        }
+        /* bootstrapURLKeys={{ key }} */
+        defaultCenter={center}
+        defaultZoom={13}
+      >
+        <Marker lat={} lng={} />
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 export default GoogleMap;
--- src/container/map.js
+++ src/container/map.js
@@ -8,7 +8,7 @@ import GoogleMapSelection from "../components/GoogleMapSelection/GoogleMapSelect
 import Footer from "../components/Footer/Footer";
 import LogoContext from "../contexts/LogoContext";
 
-function Map(props) {
+function Map({ markers }) {
   // Utilisation du LogoContext pour gérer la couleur du logo en fonction de la page où on est
   const { setLogoColor } = useContext(LogoContext);
 
@@ -59,7 +59,11 @@ function Map(props) {
           onClickButton={onClickButton}
           textInput={textInput}
         />
-        <GoogleMap markers={props} />
+        <GoogleMap
+          changeTheme={changeTheme}
+          ChangeColorTheme={ChangeColorTheme}
+          markers={markers}
+        />
       </div>
       {/* ---------------------------------------------------- */}
 
