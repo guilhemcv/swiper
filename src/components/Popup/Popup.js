f84375c5249a90fe14dfabc50afe0ae076a2abed
Coucoralie <c.cocourant@gmail.com>
2021-11-10 17:12:55 +0100

    ceation du component Popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,14 +1,30 @@
 import React from "react";
+import "./Popup.css";
 
 function Popup(props) {
-  return (
-    <div className="popup-box">
-      <div className="box">
-        <span className="close-icon" onClick={props.handleClose}></span>
-        {props.content}
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          close
+        </button>
+        {props.children}
       </div>
     </div>
+  ) : (
+    ""
   );
 }
 
 export default Popup;
+
+// function Popup(props) {
+//   return (
+//     <div className="popup-box">
+//       <div className="box">
+//         <span className="close-icon" onClick={props.handleClose}></span>
+//         {props.content}
+//       </div>
+//     </div>
+//   );
+// }
