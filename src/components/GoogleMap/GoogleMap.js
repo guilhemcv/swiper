bc91e150408319beb29564654162d7556a61e430
Guilhem SEYVET <guilhemcvdev@outlook.com>
2021-11-12 14:55:24 +0100

    maj google map avec markers fonctionnels

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,68 +1,40 @@
+/* eslint-disable no-else-return */
+/* eslint-disable consistent-return */
 /* eslint-disable array-callback-return */
 /* eslint-disable prefer-destructuring */
-import React from "react";
+import React, { useEffect } from "react";
 import GoogleMapReact from "google-map-react";
 import "./GoogleMap.css";
 import env from "react-dotenv";
 import Popup from "../Popup/Popup";
-import imageParcs from "../../Assets/Images/Markers/parc.png";
-import imageParking from "../../Assets/Images/Markers/parking.png";
-import imageMusee from "../../Assets/Images/Markers/musee.png";
-import imageCinema from "../../Assets/Images/Markers/cinema.png";
-import imageRestaurant from "../../Assets/Images/Markers/restaurant.png";
-import imageSpectacle from "../../Assets/Images/Markers/spectacle.png";
-import imagePiscine from "../../Assets/Images/Markers/piscine.png";
-import imageBicloo from "../../Assets/Images/Markers/bicloo.png";
-import imageMarguerite from "../../Assets/Images/Markers/voiture.png";
-import imageSport from "../../Assets/Images/Markers/sport.png";
-import imageRecherche from "../../Assets/Images/Markers/recherche.png";
 import customStylesLite from "./CustomStyleLite";
 import customStylesDark from "./CustomStylesDark";
 
 /* API Google */
 const key = process.env.REACT_APP_API_KEY;
 
-function Mar() {
-  return <div></div>
-};
+function Marker(props) {
+  console.log(props);
+  return (
+    <div
+      style={{
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img
+        className="marker"
+        src={props.data.img}
+        alt={props.data.type}
+        width="35px"
+      />
+    </div>
+  );
+}
 
 /* fonction affichage de la Google Map */
 function GoogleMap({ changeTheme, markers }) {
   const center = { lat: 47.212369, lng: -1.55 };
-
-
-
-  function Marker() {
-    for (let i = 0; i < markers.length; i += 1) {
-      if (markers[i].type === "parking") {
-        return (
-          <div
-            style={{
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <img
-              className="marker"
-              src={imageParking}
-              alt="parking"
-              width="35px"
-            />
-          </div>
-        );
-      }
-      if (markers[i].type === "parcs") {
-        return (
-          <div
-            style={{
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <img className="marker" src={imageParcs} alt="parcs" width="35px" />
-          </div>
-        );
-      }
-    }
-  }
+  console.log(markers);
 
   return (
     <div className="googlemap">
@@ -76,7 +48,16 @@ function GoogleMap({ changeTheme, markers }) {
         defaultCenter={center}
         defaultZoom={13}
       >
-        <Marker lat={} lng={} />
+        {markers.map((marker) => {
+          console.log("marker");
+          return (
+            <Marker
+              lat={marker.coordonnees[1]}
+              lng={marker.coordonnees[0]}
+              data={marker}
+            />
+          );
+        })}
       </GoogleMapReact>
     </div>
   );
